58bbe36418853f6e89ba4f695d59a94ca75900e5
Yongqinchuan Du <duqcyxwd@gmail.com>
2015-01-23 13:44:34 -0500

    Initial Commit

---
+++ H-Mobile RobotTests/H_Mobile_RobotTests.swift
@@ -0,0 +1,36 @@
+//
+//  H_Mobile_RobotTests.swift
+//  H-Mobile RobotTests
+//
+//  Created by Yongqinchuan Du on 2015-01-23.
+//  Copyright (c) 2015 Yongqinchuan Du. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class H_Mobile_RobotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
