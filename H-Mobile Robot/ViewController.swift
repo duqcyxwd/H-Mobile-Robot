4620cdd0cc0f1832d98ba77cf9579c85093467d3
Yongqinchuan Du <duqcyxwd@gmail.com>
2015-01-30 15:13:55 -0500

    Add Socket library to project

--- H-Mobile Robot/ViewController.swift
+++ H-Mobile Robot/ViewController.swift
@@ -8,12 +8,57 @@
 
 import UIKit
 import Foundation
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    
+    let nsMotionQueue = NSOperationQueue()
+    let motionManager = CMMotionManager()
+    
+    @IBOutlet weak var wifiConnection: UIButton!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var msgLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.    
+        var globalVar = 0
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            for var i = 0; i < 100; i++
+            {
+                println("GCD thread running.")
+//                self.xLabel.text = String(i)
+                
+                globalVar = i
+                
+                sleep(1)
+            }
+            //            sleep(5);
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                println("main")
+                NSLog("main")
+                for i in 1...10
+                {
+                    sleep(1)
+                    self.xLabel.text = String(globalVar)
+                }
+                //这里返回主线程，写需要主线程执aa行的代码
+                println("这里返回主线程，写需要主线程执行的代码")
+            })
+        })
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        motionManager.startDeviceMotionUpdatesToQueue(nsMotionQueue, withHandler: updateView)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,9 +80,49 @@ class ViewController: UIViewController {
 //        println(welcomeMessage)
 //        testudpserver()
         testudpclient()
+        self.xLabel.text = "Updated"
+//        let label =
+        
+//        self.labb.text = "update"
+        
 
         // convertedNumber 被推测为类型 "Int?"， 或者类型 "optional Int"
         
+    }
+    
+    
+    func updateView(motion: CMDeviceMotion!, error:NSError!) {
+        let grav : CMAcceleration = motion.gravity
+        
+        let x = CGFloat(grav.x)
+        let y = CGFloat(grav.y)
+        var p = CGPointMake(x,y)
+        
+        println(x)
+        println(y)
+        println(p)
+        
+        if (error != nil) {
+            NSLog("\(error)")
+        }
+        
+        // Have to correct for orientation.
+        var orientation = UIApplication.sharedApplication().statusBarOrientation
+        
+        if(orientation == UIInterfaceOrientation.LandscapeLeft) {
+            var t = p.x
+            p.x = 0 - p.y
+            p.y = t
+        } else if (orientation == UIInterfaceOrientation.LandscapeRight) {
+            var t = p.x
+            p.x = p.y
+            p.y = 0 - t
+        } else if (orientation == UIInterfaceOrientation.PortraitUpsideDown) {
+            p.x *= -1
+            p.y *= -1
+        }
+        
+        var v = CGVectorMake(p.x, 0 - p.y)
     }
     
 //    let mylabel = UILabel(frame:CGRect(origin: CGPointMake(10.0, 50.0), size: CGSizeMake(150,50)))
@@ -80,10 +165,6 @@ class ViewController: UIViewController {
     
 //    var welcomeMessage: String
 //    welcomeMessage = "Hello"
-    
-    
-    
-    
 
 }
 
