d1b57eb8370be1ead622e3b1cb9567642c1cf7a8
Yongqinchuan Du <duqcyxwd@gmail.com>
2015-02-06 23:45:28 -0500

    Add the different view of Controller and control

---
+++ H-Mobile Robot/myViewController2.swift
@@ -0,0 +1,35 @@
+//
+//  myViewController2.swift
+//  H-Mobile Robot
+//
+//  Created by Yongqinchuan Du on 2015-02-05.
+//  Copyright (c) 2015 Yongqinchuan Du. All rights reserved.
+//
+
+import UIKit
+
+class myViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
